00d9d998e5b9c77522a96369963cad4cf105c7c5
Matias Kottwitz <kottvitz.matias@gmail.com>
2024-10-12 15:34:01 -0300

    Se cargo el banner y se renderizo en el homePage

--- web-app-cafewalk/src/cafeteria/containers/BannerContainer.jsx
+++ web-app-cafewalk/src/cafeteria/containers/BannerContainer.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import './../../../public/css/HomePage.css'
+import imagenHome from '../../assets/images/home_image.jpg';
 
-export const BannerContainer = () => {
+export const BannerContainer = ({homeAlt, homeImage}) => {
   return (
-    <div>BannerContainer</div>
+   <>
+    <div id="home">
+            <div className="home-image" role="img"></div>
+            <div className="rectangle-img">
+                <img className="image-local" src={homeImage} alt={homeAlt}/>
+                <div className="home-text">
+                    <h1>Más que una <span>&nbsp; Cafetería</span>, una experiencia sensorial.</h1>
+                    <p>
+                        Tu momento de tranquilidad, nuestra mejor taza.
+                    </p>
+                </div>
+            </div>
+        </div>
+   </>
   )
 }
+BannerContainer.defaultProps = {
+  homeImage: imagenHome,
+  homeAlt: "Imagen Principal"
+};
--- web-app-cafewalk/src/cafeteria/pages/HomePage.jsx
+++ web-app-cafewalk/src/cafeteria/pages/HomePage.jsx
@@ -1,28 +1,14 @@
 import PropTypes from "prop-types";
 
 import './../../../public/css/HomePage.css'
-import imagenHome from '../../assets/images/home_image.jpg';
 
-export const HomePage = ({homeImage, homeAlt}) => {
+import { BannerContainer } from "../containers/BannerContainer";
+
+export const HomePage = () => {
   return (
     <>
-        <div id="home">
-            <div className="home-image" role="img"></div>
-            <div className="rectangle-img">
-                <img className="image-local" src={homeImage} alt={homeAlt}/>
-                <div className="home-text">
-                    <h1>Más que una <span>&nbsp; Cafetería</span>, una experiencia sensorial.</h1>
-                    <p>
-                        Tu momento de tranquilidad, nuestra mejor taza.
-                    </p>
-                </div>
-            </div>
-        </div>
+        <BannerContainer />
     </>
   )
 }
 
-HomePage.defaultProps = {
-  homeImage: imagenHome,
-  homeAlt: "Imagen Principal"
-};
