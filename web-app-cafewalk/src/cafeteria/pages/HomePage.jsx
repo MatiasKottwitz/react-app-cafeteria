d40c0e938045ba03a65c3ea8ff88f28994a58204
Matias Kottwitz <kottvitz.matias@gmail.com>
2024-10-19 21:58:48 -0300

    Se agrego un slide

--- web-app-cafewalk/src/cafeteria/pages/HomePage.jsx
+++ web-app-cafewalk/src/cafeteria/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-
+import Slideshow from './../containers/Slideshow'
 import { PopularContainer,BannerContainer, ServicesContainer, AboutContainer, MenuContainer } from "../containers";
 import {bannerData, popularMenuData, servicesData,aboutData, productCards} from './../data/data'
 
@@ -11,6 +11,7 @@ export const HomePage = () => {
         <ServicesContainer servicesData={servicesData} />
         <AboutContainer aboutData={aboutData}/>
         <MenuContainer productCards={productCards}/>
+        <Slideshow />
     </>
   )
 }
