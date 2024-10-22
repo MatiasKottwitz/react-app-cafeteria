7ff80ea3b3ad3c325bc2ea008391050ae8534b32
MatiasKottwitz <kottvitz.matias@gmail.com>
2024-10-22 06:06:31 -0300

    Se inicio trabajos en le useState para captura de datos...

--- web-app-cafewalk/src/cafeteria/containers/ContactContainer.jsx
+++ web-app-cafewalk/src/cafeteria/containers/ContactContainer.jsx
@@ -1,58 +1,87 @@
+import { useState } from 'react';
+
 import './../../../public/css/contact.css'
 
 export const ContactContainer = () => {
-  return (
+  
+    const [formState, setFormState] = useState({
+        nombre:'matias',
+        apellido:'kottwitz',
+        email: 'kottvitz@matias.com',
+        edad: '25',
+        telefono:'3764119400',
+        dropdown: 2,
+        recommend: 'Definitivamente',
+        improved: 'delivery',
+        comments: 'Soy leru leru!'
+    });
+  
+    const {nombre, apellido, email, telefono,edad, dropdown, recommend, improved, comments } = formState;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+       
+
+        setFormState ({
+            ...formState,
+            [name]:value
+        });
+
+        console.table({name, value});
+    }
+
+    return (
     <div id="contacto">
             <h3 className="contact-title" >Contacta con nosotros</h3>
             <div className="form-contact">
                 <form action="" className='form-control'>
 
                     <div className='div-form' for="nombre" >Nombre: 
-                        <input className='input-form' type="text" name="nombre" required placeholder="Ingrese su Nombre"/></div>
+                        <input className='input-form' type="text" name="nombre" required placeholder="Ingrese su Nombre"  value={nombre} onChange={onInputChange}/></div>
                     <div className='div-form' for="apellido">Apellido: 
-                        <input className='input-form' type="text" name='apellido' required placeholder="Ingrese su Apellido"/></div>
+                        <input className='input-form' type="text" name='apellido' required placeholder="Ingrese su Apellido" value={apellido} /></div>
                     <div className='div-form' for="email" >Email: 
-                        <input className='input-form' type="email" name="email" required placeholder="email@email.com"/></div>                   
+                        <input className='input-form' type="email" name="email" required placeholder="email@email.com" value={email} /></div>                   
                     <div className='div-form'  for="telefono" >Telefono: 
-                        <input className='input-form' type="text" name='telefono' required placeholder="+54"/></div>
+                        <input className='input-form' type="text" name='telefono' required placeholder="+54" value={telefono} /></div>
                     <div className='div-form'  for="edad" >Edad: 
-                        <input className='input-form' type="number" name="edad" min="18" max="50" placeholder="18"/></div>
+                        <input className='input-form' type="number" name="edad" min="18" max="50" placeholder="18" value={edad} /></div>
                                       
 
                     <div for="dropdown" className='div-form' >¿Qué opción describe mejor su función actual?
                         <select name="dropdown" id="dropdown">
-                            <option value="">Selecciona una opcion</option>
-                            <option value="1">Estudiante</option>
-                            <option value="2">Trabajo de tiempo completo</option>
-                            <option value="3">Trabajo de medio tiempo</option>
+                            <option value={dropdown}>Selecciona una opcion</option>
+                            <option value={dropdown}>Estudiante</option>
+                            <option value={dropdown}>Trabajo de tiempo completo</option>
+                            <option value={dropdown}>Trabajo de medio tiempo</option>
                         </select>
                     </div>
            
                     <div for="recommend" className='div-form' >¿Recomendarias a Caffe Walk?</div>
                         <div for="recommend" className='div-form' >
-                            <input type="radio" name="recommend" id="recommend" value="Definitivamente" className="input-form inline"/>Definitivamente
+                            <input type="radio" name="recommend" id="recommend" value={recommend} className="input-form inline"/>Definitivamente
                         </div>
                         <div for="recommend" className='div-form' >
-                            <input type="radio" name="recommend" id="recommend" value="TalVez" className="inline input-form"/>Tal Vez
+                            <input type="radio" name="recommend" id="recommend" value={recommend} className="inline input-form"/>Tal Vez
                         </div>
                         <div for="recommend" className='div-form' >
-                            <input type="radio" name="recommend" id="recommend" value="No" className=" input-form inline"/>Not creo..
+                            <input type="radio" name="recommend" id="recommend" value={recommend} className=" input-form inline"/>Not creo..
                         </div>
             
                     <div for="improved" className='div-form' >¿En que puesto te gustaria aplicar?
                         <div for=""> 
-                            <input type="checkbox" name="improved" value="pasteleria" className="input-form inline"/>Pasteleria
+                            <input type="checkbox" name="improved" value={improved} className="input-form inline"/>Pasteleria
                         </div>
                         <div for="">
-                            <input type="checkbox" name="improved" value="preparacion" className="input-form  inline"/>Preparacion de Cafe
+                            <input type="checkbox" name="improved" value={improved}  className="input-form  inline"/>Preparacion de Cafe
                         </div>
                         <div for="">
-                            <input type="checkbox" name="improved" value="delivery" className=" input-form inline"/>Cadete de delivery
+                            <input type="checkbox" name="improved" value={improved}  className=" input-form inline"/>Cadete de delivery
                         </div>
                     </div>
                 
                     <div for="comments" className='div-form' >Cuentanos mas acerca de ti:
-                        <textarea name="comments" id="comments" cols="30" rows="3" placeholder="Aprovecha para contarnos acerca de ti y tus dudas." ></textarea>
+                        <textarea name="comments" id="comments" cols="30" rows="3" value={comments} placeholder="Aprovecha para contarnos acerca de ti y tus dudas." ></textarea>
                     </div>
                     
                     <input className='input-form' type="submit" id="submit"/>
